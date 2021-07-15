209a51874c215b8d70f9d889c4c8937336fba055
SeungRhee Oh <sroh@SR5HMBA.local>
2021-07-15 15:18:50 +0900

    Make table data structure.

--- src/data-structures.lisp
+++ src/data-structures.lisp
@@ -48,3 +48,66 @@
   (funcall (funcall queue 'call-each) proc))
 
 ;;; table
+(defun make-table ()
+  (let ((local-table (list '*table*)))
+    (labels ((_insertf (keys value)
+	       (let ((record (assoc (car keys) (cdr local-table))))
+		   (if record
+		       (setf (cdr record) value)
+		       (setf (cdr local-table) (cons (cons keys value)
+						     (cdr local-table))))))
+	     (_dispatch (m)
+	       (cond ((eq m 'insert) #'_insertf))))
+      #'_dispatch)))
+	       
+  
+(defun make-table (&key (name nil))
+  (let ((local-table (list name)))
+    (labels ((__insertf-kv (key value table &key (proc #'cons))
+	       (let ((record (assoc key (cdr table))))
+		 (if record
+		     (setf (cdr record) (funcall proc value (cons (cdr record) nil)))
+		     (setf (cdr table) (cons (cons key value) (cdr table))))
+		 table))
+	     (__insertf-multiple-key (keys value &optional (table local-table) (old-keys nil))
+	       (print local-table)
+	       (cond ((null keys) table)
+		     ((null (cdr keys))
+		      (__insertf-kv (car keys) value table)
+		      (__insertf-multiple-key (cdr keys) value table (cons (car keys) old-keys)))
+		     (t (let ((sub-table (assoc (car keys) (cdr table))))
+			  (cond (sub-table
+				 (__insertf-multiple-key (cdr keys) value sub-table (cons (car keys) old-keys)))
+				(t
+				 (let ((s-table (list (car keys))))
+				   (setf (cdr table) (cons s-table (cdr table)))
+				   (__insertf-multiple-key (cdr keys) value s-table (cons (car keys) old-keys)))))))))
+
+	     (_insert (keys value)
+	       (__insertf-multiple-key keys value)
+	       #'_dispatch)
+	     (_lookup (keys &optional (table local-table))
+	       (cond ((null keys) (cdr table))
+		     (t (let ((r (assoc (car keys) (cdr table))))
+			  (cond (r (_lookup (cdr keys) r))
+				(t nil))))))
+	     (_dispatch (m)
+	       (cond ((eq m 'insert) #'_insert)
+		     ((eq m 'lookup) #'_lookup))))
+      #'_dispatch)))
+
+
+
+
+      ;; (__insertf-multiple-key '(2 1 0 3) '(c d h s))
+      ;; (__insertf-multiple-key '(2 1 0 1) '(d d h d))
+      ;; (__insertf-multiple-key '(1 2 0 1) '(d d h d))
+      ;; (__insertf-multiple-key '(1 1 0 1) '(d d h d))
+      ;; (__insertf-multiple-key '(0 1 0 1) '(h d h d))
+      ;; (__insertf-multiple-key '(1 0 0 1) '(d h h d))
+      ;; (__insertf-multiple-key '(3 1 0 1) '(s d h d))
+      ;; (__insertf-multiple-key '(2 2 2 1) '(c c c c))
+      ;; (__insertf-multiple-key '(2 2 2 1) '(c c c d))
+	     
+      ;; (_lookup '(2 2 2 1))
+
